5279dd48eca0b469a92e7ca95e0864c1ce0abe04
Allison Snipes <ajsnipes@usfca.edu>
2019-06-10 20:30:47 -0700

    working together

--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,13 @@
 // CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
 // *********************************************************************************
 // Dependencies
-const environment = process.env.NODE_ENV || 'development'
-const config = require('../knexfile')
-const environmentConfig = config[environment]
+const ENV = process.env.NODE_ENV || 'development'
 /* eslint-disable  no-unused-vars */
 const mysql = require('mysql')
 /* eslint-enable  no-unused-vars */
 
 // Creates mySQL connection using Knex.js
-const Knex = require('knex')
-const connection = Knex(environmentConfig)
+const Knex = require('knex')(require('../knexfile')[ENV])
 
 // Exports the connection for other files to use working changes
-module.exports = connection
+module.exports = Knex
